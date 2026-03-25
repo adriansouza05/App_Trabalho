53e1c9727ffe5858da353bed32dd6823ebbbc812
Adrian Souza Teixeira <adriansouzateixeira777@gmail.com>
2026-03-25 19:14:57 -0300

    Esqueleto inicial do flutter.
    Adicionei as pastas, o esqueleto inicial na main e a biblioteca que vamos utilizar para visualizar o device preview.
    
    comandos para colocar no terminal:
    flutter pub get
    flutter pub upgrade
    flutter pub add device_preview
    flutter pub add get_it

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_trabalho
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() {
+  runApp(DevicePreview(builder: (context) => const MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
